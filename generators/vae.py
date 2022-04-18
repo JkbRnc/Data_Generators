27f978674c4382add1c78ce338e89fc4c7cc1272
rencjaku <rencjaku@fit.cvut.cz>
2022-04-18 18:58:38 +0200

    Privatize atributes

--- generators/vae.py
+++ generators/vae.py
@@ -2,10 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-import math
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sn
 
 import numpy as np
 
@@ -25,13 +22,13 @@ class Encoder(nn.Module):
         ]
         input = layer
 
-      self.model = nn.Sequential(*hidden_layers)
-      self.mean = nn.Linear(input, latent_dim)
-      self.logvar = nn.Linear(input, latent_dim)
+      self.__model = nn.Sequential(*hidden_layers)
+      self.__mean = nn.Linear(input, latent_dim)
+      self.__logvar = nn.Linear(input, latent_dim)
 
   def forward(self, x):
-    hidden = self.model(x)
-    return self.mean(hidden), self.logvar(hidden)
+    hidden = self.__model(x)
+    return self.__mean(hidden), self.__logvar(hidden)
 
 class Decoder(nn.Module):
   def __init__(self, output_dim, input_dim = 32, hidden_params = [256, 256]):
@@ -47,17 +44,17 @@ class Decoder(nn.Module):
         input = layer
       
       hidden_layers += [nn.Linear(input, output_dim)]
-      self.model = nn.Sequential(*hidden_layers)
+      self.__model = nn.Sequential(*hidden_layers)
 
   def forward(self, x):
-    return self.model(x)
+    return self.__model(x)
 
 class VAE():
   def __init__(self, latent_dim = 32, e_params = [256, 256], d_params = [256, 256], num_epochs = 300, batch_size = 512, lr = 1e-3):
-    self.encoder = None
-    self.decoder = None
-    self.continous_scaler = StandardScaler()
-    self.categorical_scaler = OneHotEncoder()
+    self.__encoder = None
+    self.__decoder = None
+    self.__continous_scaler = StandardScaler()
+    self.__categorical_scaler = OneHotEncoder()
     self.latent_dim = latent_dim
     self.e_params = e_params
     self.d_params = d_params
@@ -67,25 +64,26 @@ class VAE():
 
   def fit(self, data, categorical_columns = []):
     continous, categorical = self.__split(data, categorical_columns)
-    self.categorical_columns = categorical_columns
-    self.continous_columns = list(continous.columns)
+    self.__categorical_columns = categorical_columns
+    self.__continous_columns = list(continous.columns)
+    self.columns = self.__continous_columns + self.__categorical_columns
 
-    self.continous_scaler.fit(continous)
-    self.categorical_scaler.fit(categorical)
+    self.__continous_scaler.fit(continous)
+    self.__categorical_scaler.fit(categorical)
     continous, categorical = self.__transform(continous, categorical)
-    d1 = pd.DataFrame(continous, columns=self.continous_columns)
+    d1 = pd.DataFrame(continous, columns=self.__continous_columns)
     data = d1.join(
-        pd.DataFrame(categorical, columns=self.categorical_scaler.get_feature_names_out())
+        pd.DataFrame(categorical, columns=self.__categorical_scaler.get_feature_names_out())
     )
 
     self.data_dim = data.shape[1]
-    self.encoder = Encoder(self.data_dim, self.latent_dim, self.e_params)
-    self.decoder = Decoder(self.data_dim, self.latent_dim, self.d_params)
+    self.__encoder = Encoder(self.data_dim, self.latent_dim, self.e_params)
+    self.__decoder = Decoder(self.data_dim, self.latent_dim, self.d_params)
 
     self.__train(data)
 
   def __transform(self, continous, categorical):
-    return self.continous_scaler.transform(continous), self.categorical_scaler.transform(categorical).toarray().astype(np.float64)
+    return self.__continous_scaler.transform(continous), self.__categorical_scaler.transform(categorical).toarray().astype(np.float64)
 
   def __split(self, data, categorical_columns):
     categorical = data[categorical_columns]
@@ -95,24 +93,26 @@ class VAE():
   def __train_loader(self, data):    
     d = torch.tensor(data.values)
     d = d.type(dtype=torch.float32)
-    d_length = d.size(dim=0)
-    d_labels = torch.zeros(d_length)
+
     train_loader = torch.utils.data.DataLoader(
         d, batch_size=self.batch_size, shuffle=True
     )
     return train_loader
 
   def __train(self, data):
-    optimizer = torch.optim.Adam(list(self.encoder.parameters()) + list(self.decoder.parameters()), lr = self.lr, weight_decay=1e-6)
+    optimizer = torch.optim.Adam(
+        list(self.__encoder.parameters()) + list(self.__decoder.parameters()),
+        lr = self.lr,
+        weight_decay=1e-6)
     self.training = True
     data_loader = self.__train_loader(data)
 
     for epoch in range(self.num_epochs):
       for i, batch in enumerate(data_loader):
         optimizer.zero_grad()
-        mu, logvar = self.encoder(batch)
+        mu, logvar = self.__encoder(batch)
         z = self.__reparametrize(mu, logvar)
-        reconstruction = self.decoder(z)
+        reconstruction = self.__decoder(z)
 
         loss = self.__loss_function(batch, reconstruction, mu, logvar)
         loss.backward()
@@ -137,14 +137,15 @@ class VAE():
     self.training = False
     with torch.no_grad():
       latent_samples = torch.randn(quantity, self.latent_dim)
-      generated_samples = self.decoder(latent_samples)
-      df = pd.DataFrame(generated_samples, columns = list(self.continous_columns) + list(self.categorical_scaler.get_feature_names_out()))
-      continous, categorical = self.__split(df, self.categorical_scaler.get_feature_names_out())
-      if self.continous_columns:
-        scaled_continous = self.continous_scaler.inverse_transform(continous)
-        if self.categorical_columns:
-          scaled_categorical = self.categorical_scaler.inverse_transform(categorical)
+      generated_samples = self.__decoder(latent_samples)
+      df = pd.DataFrame(generated_samples, columns = list(self.__continous_columns) + list(self.__categorical_scaler.get_feature_names_out()))
+      continous, categorical = self.__split(df, self.__categorical_scaler.get_feature_names_out())
+      
+      if self.__continous_columns:
+        scaled_continous = self.__continous_scaler.inverse_transform(continous)
+        if self.__categorical_columns:
+          scaled_categorical = self.__categorical_scaler.inverse_transform(categorical)
           return np.concatenate((scaled_continous, scaled_categorical), axis=1)
         return scaled_continous
-      scaled_categorical = self.categorical_scaler.inverse_transform(categorical)
+      scaled_categorical = self.__categorical_scaler.inverse_transform(categorical)
       return scaled_categorical
